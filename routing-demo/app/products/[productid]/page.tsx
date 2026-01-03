82479e2885d2d054d92144868bb6406c2bd53a3d
Aman Soni <aman2709asgmail.com>
2026-01-03 12:13:31 +0530

    Convert routing-demo from submodule to normal folder

---
+++ routing-demo/app/products/[productid]/page.tsx
@@ -0,0 +1,7 @@
+export default async function ProductDetails
+({params}:{params:Promise<{productid:string}>;}){
+    
+    const productId=(await params).productid;
+    return <h1>Details about product {productId}</h1>
+}
+
